7bd6b57416006cc2ee46187d14a12fecd2bb381b
alvarotellez <alvarotellezhidalgo@gmail.com>
2017-02-15 12:36:10 +0100

    Bbyyyyyyyyyyyy <3333

--- app/src/main/java/es/iesnervion/atellez/sqlite_android/MainActivity.java
+++ app/src/main/java/es/iesnervion/atellez/sqlite_android/MainActivity.java
@@ -11,6 +11,8 @@ import android.widget.Toast;
 import java.util.Calendar;
 import java.util.Date;
 
+import belka.us.androidtoggleswitch.widgets.ToggleSwitch;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText edCantidad, edNota, edTipo;
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Float cantidad;
     String nota, tipo, fechaCreacion;
     SQLiteDatabase db;
+    ToggleSwitch toggleSwitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,8 +28,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         edCantidad = (EditText) findViewById(R.id.etCantidad);
         edNota = (EditText) findViewById(R.id.etNota);
-        edTipo = (EditText) findViewById(R.id.etTipo);
+
         btnGuardar = (Button) findViewById(R.id.btnGuardar);
+        toggleSwitch = (ToggleSwitch) findViewById(R.id.swGI);
+
         btnGuardar.setOnClickListener(this);
 
         AnotacionSQLiteHelper nuevaAnotacion = new AnotacionSQLiteHelper(this, "DBAnotacion", null, 1);
@@ -40,7 +45,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
            case R.id.btnGuardar:
                cantidad =  Float.parseFloat(edCantidad.getText().toString()) ;
                nota = edNota.getText().toString();
-               tipo = edTipo.getText().toString();
+
                //Obtenemos la fecha actual del sistema
                Calendar cc = Calendar.getInstance();
                int year=cc.get(Calendar.YEAR);
@@ -48,6 +53,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                int month=cc.get(Calendar.MONTH)+1;
                int mDay = cc.get(Calendar.DAY_OF_MONTH);
                fechaCreacion = mDay +":"+month+":"+year;
+
+               int posicionToggle = toggleSwitch.getCheckedTogglePosition();
+                //Posicion 0 --> Gasto
+               //Posicion 1 --> Ingreso
+
+               if(posicionToggle ==0){
+                   tipo = "GASTO";
+               }else{
+                   tipo = "INGRESO";
+               }
 
                if (!(cantidad>0 && nota.isEmpty() && tipo.isEmpty() && fechaCreacion.isEmpty())){
 
@@ -57,7 +72,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                    Toast.makeText(this, "La anotación se ha creado con exito",Toast.LENGTH_SHORT).show();
                    edCantidad.setText(" ");
                    edNota.setText(" ");
-                   edTipo.setText(" ");
                }else {
                    Toast.makeText(this, "La anotación no se ha podido crear",Toast.LENGTH_SHORT).show();
                }
